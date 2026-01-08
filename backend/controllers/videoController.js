c6677c751e2c6ac57ce38d6994cefb486af74447
Ridhdhi11 <motkariddhi6711@gmail.com>
2026-01-08 14:39:40 +0530

    Update video controller logic

--- backend/controllers/videoController.js
+++ backend/controllers/videoController.js
@@ -205,7 +205,255 @@ const videos = [
       _id: "c4",
       name: "Chef Bharat"
     }
-  }
+  },
+  // ================= Bengali =================
+  {
+    _id: "v14",
+    title: "Sandesh",
+    description: "Easy & Special Diwali Sweets",
+    youtubeEmbedUrl: "https://www.youtube.com/embed/Ag9Id4Wiv90",
+    isFree: true,
+    category: {
+      name: "Bengali",
+      slug: "bengali"
+    },
+    chef: {
+      _id: "c5",
+      name: "Chef Kunal Kapur"
+    }
+  },
+  {
+    _id: "v15",
+    title: "Rasgulla",
+    description: "Tips for Soft Roshogullas",
+    youtubeEmbedUrl: "https://www.youtube.com/embed/dd9_YZdQS0c",
+    isFree: true,
+    category: {
+      name: "Bengali",
+      slug: "bengali"
+    },
+    chef: {
+      _id: "c5",
+      name: "Chef Sanjyot Keer"
+    }
+  },
+  // ================= Kashmiri =================
+  {
+    _id: "v16",
+    title: "Dum Aloo",
+    description: "No Onion No Garlic",
+    youtubeEmbedUrl: "https://www.youtube.com/embed/ZBwB53EvrRo",
+    isFree: true,
+    category: {
+      name: "Kashmiri",
+      slug: "kashmiri"
+    },
+    chef: {
+      _id: "c6",
+      name: "Chef Sanjyot Keer"
+    }
+  },
+  {
+    _id: "v17",
+    title: "Mutton Rogan Josh",
+    description: "Kashmiri Style Roghan Josh",
+    youtubeEmbedUrl: "https://www.youtube.com/embed/XVgZBsuNU60",
+    isFree: true,
+    category: {
+      name: "Kashmiri",
+      slug: "kashmiri"
+    },
+    chef: {
+      _id: "c6",
+      name: "Chef Ranveer Brar"
+    }
+  },
+  // ================= Maharashtrian =================
+  {
+    _id: "v18",
+    title: "Vada Pav",
+    description: "Mumbai Vada Pav",
+    youtubeEmbedUrl: "https://www.youtube.com/embed/9vB0AKrWw50",
+    isFree: true,
+    category: {
+      name: "Maharashtrian",
+      slug: "maharashtrian"
+    },
+    chef: {
+      _id: "c7",
+      name: "Chef Ranveer Brar"
+    }
+  },
+  {
+    _id: "v19",
+    title: "Misal Pav",
+    description: "Kolhapur style spicy Misal recipe",
+    youtubeEmbedUrl: "https://www.youtube.com/embed/R1AfI17BzZs",
+    isFree: true,
+    category: {
+      name: "Maharashtrian",
+      slug: "maharashtrian"
+    },
+    chef: {
+      _id: "c7",
+      name: "Chef Ranveer Brar"
+    }
+  },
+  // ================= Rajasthani =================
+  {
+    _id: "v20",
+    title: "Dal Baati",
+    description: "Rajasthani Dal Baati Churma",
+    youtubeEmbedUrl: "https://www.youtube.com/embed/mZ4xTelTAGk",
+    isFree: true,
+    category: {
+      name: "Rajasthani",
+      slug: "rajasthani"
+    },
+    chef: {
+      _id: "c8",
+      name: "Chef Sanjyot Keer"
+    }
+  },
+  {
+    _id: "v21",
+    title: "Easy Ghevar",
+    description: "Diwali Special Sweets",
+    youtubeEmbedUrl: "https://www.youtube.com/embed/Bh87si4LpLI",
+    isFree: true,
+    category: {
+      name: "Rajasthani",
+      slug: "rajasthani"
+    },
+    chef: {
+      _id: "c8",
+      name: "Chef Sanjyot Keer"
+    }
+  },
+  // ================= Japanese =================
+  {
+    _id: "v22",
+    title: "Vegan Ramen",
+    description: "Spicy Noodle Soup",
+    youtubeEmbedUrl: "https://www.youtube.com/embed/plTuwZva37k",
+    isFree: true,
+    category: {
+      name: "Japanese",
+      slug: "japanese"
+    },
+    chef: {
+      _id: "c9",
+      name: "Chef Sanjyot Keer"
+    }
+  },
+  {
+    _id: "v23",
+    title: "Vegetable Tempura",
+    description: "Monsoon Japanese Recipes",
+    youtubeEmbedUrl: "https://www.youtube.com/embed/jXgPO0ZBBaU",
+    isFree: true,
+    category: {
+      name: "Japanese",
+      slug: "japanese"
+    },
+    chef: {
+      _id: "c9",
+      name: "Chef Sanjyot Keer"
+    }
+  },
+  // ================= Mediterranean =================
+  {
+    _id: "v24",
+    title: "Hummus",
+    description: "Hummus with Chole Chickpeas at Home",
+    youtubeEmbedUrl: "https://www.youtube.com/embed/V98MgC_MUUE",
+    isFree: true,
+    category: {
+      name: "Mediterranean",
+      slug: "mediterranean"
+    },
+    chef: {
+      _id: "c10",
+      name: "Chef Kunal Kapur"
+    }
+  },
+  {
+    _id: "v25",
+    title: "Falafel",
+    description: "Falafel Shawarma",
+    youtubeEmbedUrl: "https://www.youtube.com/embed/AIczG0gl1-I",
+    isFree: true,
+    category: {
+      name: "Mediterranean",
+      slug: "mediterranean"
+    },
+    chef: {
+      _id: "c10",
+      name: "Chef Sanjyot Keer"
+    }
+  },
+  // ================= Mexican =================
+  {
+    _id: "v26",
+    title: "Paneer Burrito",
+    description: "Veg Mexican Burrito Bowl ",
+    youtubeEmbedUrl: "https://www.youtube.com/embed/v3FGarkITTQ",
+    isFree: true,
+    category: {
+      name: "Mexican",
+      slug: "mexican"
+    },
+    chef: {
+      _id: "c11",
+      name: "Chef Sanjyot Keer"
+    }
+  },
+  {
+    _id: "v27",
+    title: "Mexican Rice",
+    description: "Mexican Quick Rice Recipe",
+    youtubeEmbedUrl: "https://www.youtube.com/embed/B-VivdiyL9c",
+    isFree: true,
+    category: {
+      name: "Mexican",
+      slug: "mexican"
+    },
+    chef: {
+      _id: "c11",
+      name: "Chef Sanjyot Keer"
+    }
+  },
+  // ================= Thai =================
+  {
+    _id: "v28",
+    title: "Veg Green Thai Curry",
+    description: "Home-made Thai Curry Paste",
+    youtubeEmbedUrl: "https://www.youtube.com/embed/-zeV6vSS0mk",
+    isFree: true,
+    category: {
+      name: "Thai",
+      slug: "thai"
+    },
+    chef: {
+      _id: "c12",
+      name: "Chef Sanjyot Keer"
+    }
+  },
+  {
+    _id: "v29",
+    title: "Veg Red Thai Curry",
+    description: "Homemade Thai Curry paste",
+    youtubeEmbedUrl: "https://www.youtube.com/embed/GWBxYDLRpD8",
+    isFree: true,
+    category: {
+      name: "Thai",
+      slug: "thai"
+    },
+    chef: {
+      _id: "c12",
+      name: "Chef Sanjyot Keer"
+    }
+  },
 ];
 
 // ===============================
